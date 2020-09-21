c28fbf439563b0d58238b882d8fa828fd9a19acd
ChrisKnowsDev <chrisharley81@gmail.com>
2020-09-21 14:02:01 -0400

    Banner component displaying random image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,6 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import colors from '../utilities/cssVariables'
 import React, { useState, useEffect } from 'react'
 import axios from '../utilities/axios'
 import requests from '../utilities/request'
@@ -8,16 +11,29 @@ function Banner() {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.fetchAdventure)
-      console.log(
+      setMovie(
         request.data.results[
           Math.floor(Math.random() * request.data.results.length - 1)
         ]
       )
+      return request
     }
     fetchData()
   }, [])
 
-  return <div></div>
+  return (
+    <header
+      className='banner'
+      css={css`
+        background-image: url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}');
+        background-size: cover;
+        background-position: center center;
+        height: 450px;
+      `}
+    >
+      <div className='banner-contents'></div>
+    </header>
+  )
 }
 
 export default Banner
